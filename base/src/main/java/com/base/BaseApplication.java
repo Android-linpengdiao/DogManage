f0d1937e50c86b126091b4abf12d22037d4d6d26
linpengdiao <742094154@qq.com>
2022-04-15 07:21:14 +0800

    基础

---
+++ base/src/main/java/com/base/BaseApplication.java
@@ -0,0 +1,77 @@
+package com.base;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.base.utils.CommonUtil;
+import com.base.utils.MsgCache;
+
+public class BaseApplication extends Application {
+
+    private static BaseApplication myApplication;
+    private static Handler handler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        myApplication = this;
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    public static BaseApplication getInstance() {
+        return myApplication;
+    }
+
+    public static Handler getHandler() {
+        return handler;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        MsgCache.get(this).put(Constants.USER_INFO, userInfo);
+    }
+
+    public UserInfo getUserInfo() {
+        UserInfo userinfo = (UserInfo) MsgCache.get(this).getAsObject(Constants.USER_INFO);
+        if (!CommonUtil.isBlank(userinfo)) {
+            return userinfo;
+        }
+        return new UserInfo();
+    }
+
+    public String getMetaData(String property) {
+        String out = "";
+
+        if (property.equals("versionCode")) {
+            try {
+                PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+                return String.valueOf(pi.versionCode);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }if (property.equals("versionName")) {
+            try {
+                PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+                return pi.versionName;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (property.equals("CHANNEL")) {
+            try {
+                ApplicationInfo ai = getPackageManager()
+                        .getApplicationInfo(getPackageName(),
+                                PackageManager.GET_META_DATA);
+                Bundle b = ai.metaData;
+                return (String) b.get("CHANNEL");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return out;
+    }
+}
---
+++ base/src/main/java/com/base/utils/GlideLoader.java
@@ -0,0 +1,241 @@
+package com.base.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import com.base.R;
+import com.base.view.GlideBlurTransformation;
+import com.base.view.GlideRoundTransform;
+import com.base.view.RoundedCornersTransform;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+
+public class GlideLoader {
+
+    public static void LoderImage(Context context, String url, ImageView view) {
+        LoderImage(context, url, view, 0);
+    }
+
+    public static void LoderImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, round))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderStaffImage(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderBannerImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, round))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderLinkImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, round))
+                .placeholder(R.mipmap.app_icon)
+                .error(R.mipmap.app_icon)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderAlbumImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, round))
+//                .placeholder(R.mipmap.upload)
+//                .error(R.mipmap.upload)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderCircleImage(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, 100))
+                .placeholder(R.drawable.head)
+                .error(R.drawable.head)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderBlurImage(Context context, String url, ImageView view) {
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .transform(new GlideBlurTransformation(context));
+        Glide.with(context)
+                .load(url)
+                .apply(requestOptions)
+                .into(view);
+    }
+
+    public static void LoderImageUrl(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .placeholder(R.drawable.head)
+                .error(R.drawable.head)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderVideoCover(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .centerInside()
+                .placeholder(R.color.black)
+                .error(R.color.black)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderRoundedImage(Context context, String url, ImageView view, int round) {
+        RoundedCornersTransform transform = new RoundedCornersTransform(context, CommonUtil.dip2px(context, round));
+        transform.setNeedCorner(true, true, false, false);
+        Glide.with(context)
+                .load(url)
+                .placeholder(R.drawable.button_top_gray)
+                .error(R.drawable.button_top_gray)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .transform(transform)
+                .into(view);
+
+    }
+
+    public static void LoderClipImage(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .centerInside()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderImageOrGif(Context context, String url, ImageView view) {
+        if (url.endsWith("gif")) {
+            Glide.with(context)
+                    .asGif()
+                    .load(url)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(view);
+        } else {
+            Glide.with(context)
+                    .load(url)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(view);
+        }
+    }
+
+    public static void LoderGalleryImage(Context context, String url, ImageView view) {
+        if (!CommonUtil.isBlank(url)) {
+            LoderGalleryImage(context, url.startsWith("http") ? url : "file://" + url, view, 0);
+        }
+    }
+
+    public static void LoderGalleryImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load(url)
+                .fitCenter()
+                .thumbnail(0.1f)
+                .placeholder(R.drawable.web_ic_picture_default)
+                .error(R.drawable.web_ic_picture_default)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderMedia(Context context, String url, ImageView view) {
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, 0))
+                .placeholder(R.color.media_color)
+                .error(R.color.media_color)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderLoadImage(Context context, String url, ImageView view) {
+        LoderLoadImage(context, url, view, 0);
+    }
+
+    public static void LoderLoadImage(Context context, String url, ImageView view, int round) {
+        Glide.with(context)
+                .load("file://" + url)
+                .centerCrop()
+                .transform(new GlideRoundTransform(context, round))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderDrawable(Context context, int drawable, ImageView view) {
+        Glide.with(context)
+                .load(drawable)
+                .fitCenter()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderDrawable(Context context, int drawable, ImageView view, int round) {
+        Glide.with(context)
+                .load(drawable)
+                .fitCenter()
+                .transform(new GlideRoundTransform(context, round))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
+    }
+
+    public static void LoderDrawableVip(Context context, int drawable, ImageView view, int round) {
+        RoundedCornersTransform transform = new RoundedCornersTransform(context, CommonUtil.dip2px(context, round));
+        transform.setNeedCorner(true, true, false, false);
+        Glide.with(context)
+                .load(drawable)
+                .fitCenter()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .transform(transform)
+                .into(view);
+    }
+
+    public static Bitmap load(Context context, String url) {
+        try {
+            return Glide.with(context)
+                    .asBitmap()
+                    .load(url)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                    .get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
